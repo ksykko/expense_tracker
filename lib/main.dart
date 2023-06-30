d1bd86bf04d99c8897f991249383a6febcb1c59c
Kendrick Sagala <skendrick@teratomo.com>
2023-06-30 15:18:50 +0800

    Added theme

--- lib/main.dart
+++ lib/main.dart
@@ -5,13 +5,35 @@ var kColorScheme = ColorScheme.fromSeed(
   seedColor: const Color.fromARGB(255, 96, 59, 181),
 );
 
+var kColorSchemeDark = ColorScheme.fromSeed(
+  brightness: Brightness.dark,
+  seedColor: const Color.fromARGB(255, 5, 99, 125),
+);
+
 void main() {
   runApp(
     MaterialApp(
+      darkTheme: ThemeData.dark().copyWith(
+        useMaterial3: true,
+        colorScheme: kColorSchemeDark,
+        scaffoldBackgroundColor: const Color.fromARGB(221, 22, 22, 22),
+        cardTheme: const CardTheme().copyWith(
+          color: kColorSchemeDark.secondaryContainer,
+          margin: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+          elevation: 3,
+        ),
+        elevatedButtonTheme: ElevatedButtonThemeData(
+          style: ElevatedButton.styleFrom(
+              backgroundColor: kColorSchemeDark.primaryContainer,
+              foregroundColor: kColorSchemeDark.onPrimaryContainer),
+        ),
+      ),
       theme: ThemeData().copyWith(
         useMaterial3: true,
         colorScheme: kColorScheme,
-        appBarTheme: const AppBarTheme().copyWith(backgroundColor: kColorScheme.onSecondaryContainer, foregroundColor: kColorScheme.primaryContainer),
+        appBarTheme: const AppBarTheme().copyWith(
+            backgroundColor: kColorScheme.onSecondaryContainer,
+            foregroundColor: kColorScheme.primaryContainer),
         cardTheme: const CardTheme().copyWith(
           color: kColorScheme.secondaryContainer,
           margin: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
@@ -21,9 +43,13 @@ void main() {
           style: ElevatedButton.styleFrom(backgroundColor: kColorScheme.primaryContainer),
         ),
         textTheme: ThemeData().textTheme.copyWith(
-              titleLarge: TextStyle(fontWeight: FontWeight.bold, color: kColorScheme.onSecondaryContainer, fontSize: 20),
+              titleLarge: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  color: kColorScheme.onSecondaryContainer,
+                  fontSize: 16),
             ),
       ),
+      themeMode: ThemeMode.system,
       home: const Expenses(),
     ),
   );
